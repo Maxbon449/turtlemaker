7b928c9638987693591fa1d42bdf5e1f568f621e
Maxbon449 <rko20020125@gmail.com>
2025-06-03 21:20:51 +0900

    운동하기 메서드까지 구현

--- TurtleMaker.py
+++ TurtleMaker.py
@@ -1,6 +1,7 @@
 import turtle as t
+import random
 
-hungry = 0 # 0 ~ 5 까지. 0 = 매우 배고픔, 5 = 매우 배부름
+hungry = 2 # 0 ~ 4 까지. 0 = 매우 배고픔, 4 = 매우 배부름
 feeling = 0 # -2 ~ 2 까지. -2 = 매우 나쁨, 2 = 매우 좋음
 speed = 1.0
 
@@ -12,11 +13,70 @@ def Menu():
     n = int(input())
     return n
 
+def Better():
+    global feeling
+    if feeling != 2:
+        print("기분이 좋아집니다.")
+        feeling += 1
+def Worse():
+    global feeling
+    if feeling != -2:
+        print("기분이 나빠집니다.")
+        feeling -= 1
+
+
+def Eat():
+    global hungry
+    
+    if 0 <= hungry <= 1:
+        hungry += 1
+        Better()
+        
+    elif 2 <= hungry <= 3:
+        hungry += 1
+    else:
+        print("너무 배부릅니다.")
+        Worse()
+
+def Exercise():
+    global hungry
+    global speed
+
+    if hungry == 0:
+        print("운동을 할 힘이 없습니다.")
+        Worse()
+        return
+    
+    n = random.randint(1,6)
+    
+    if n == 1:
+        print("완벽한 운동을 해냈습니다")
+        speed += 1
+        hungry -= 1
+    else:
+        print("열심히 운동을 했습니다.")
+        speed += 0.5
+        hungry -= 1
+
+def walk():
+    n = random.randint(1,6)
+
+# def end():
+
 t.shape('turtle')
 
 while True:
     n = Menu()
-    t.forward(10)
+    
+    if n == 1:
+        Eat()
+    elif n == 2:
+        Exercise()
+    
+    
+    
+
+    
     
     
 
